d86dfff0db0dcc61de0bafae59090138ca63e455
AdeDom <pathiphonyw@gmail.com>
2020-02-13 16:10:47 +0700

    dru debug count is read

--- app/src/main/java/com/comsci/druchat/ui/main/chat/ChatAdapter.kt
+++ app/src/main/java/com/comsci/druchat/ui/main/chat/ChatAdapter.kt
@@ -1,19 +1,26 @@
 package com.comsci.druchat.ui.main.chat
 
+import android.graphics.Typeface
+import android.view.View
 import com.adedom.library.extension.loadCircle
 import com.adedom.library.util.BaseRecyclerViewAdapter
 import com.comsci.druchat.R
-import com.comsci.druchat.data.models.User
+import com.comsci.druchat.data.models.ChatUser
 import com.comsci.druchat.util.KEY_DEFAULT
 import kotlinx.android.synthetic.main.item_chat_list.view.*
 
-class ChatAdapter : BaseRecyclerViewAdapter<User>({ R.layout.item_chat_list },
+class ChatAdapter : BaseRecyclerViewAdapter<ChatUser>({ R.layout.item_chat_list },
     { holder, position, items ->
         val user = items[position]
         if (user.imageURL != KEY_DEFAULT) holder.itemView.mImgProfile.loadCircle(user.imageURL)
         holder.itemView.mTvName.text = user.name
 
-//            holder.mTvCount.text = count.toString()
-//            holder.mTvName.setTypeface(null, Typeface.BOLD)
+        if (user.count == 0) {
+            holder.itemView.mTvCount.visibility = View.GONE
+        } else {
+            holder.itemView.mTvCount.visibility = View.VISIBLE
+            holder.itemView.mTvCount.text = user.count.toString()
+            holder.itemView.mTvName.setTypeface(null, Typeface.BOLD)
+        }
     })
 
