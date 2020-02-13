1907f0ca1412d7e808e91f312de92771f119f2cb
AdeDom <pathiphonyw@gmail.com>
2020-02-14 01:36:18 +0700

    dru debug test

---
+++ app/src/test/java/com/comsci/druchat/PhoneDialogUnitTest.kt
@@ -0,0 +1,16 @@
+package com.comsci.druchat
+
+import org.junit.Test
+
+class PhoneDialogUnitTest {
+
+    @Test
+    fun phoneTest(){
+        var phone = "0946609055"
+        phone = phone.substring(1,10)
+
+        println(phone)
+
+    }
+
+}
